25a2a8ef9dc4acc30e1eb68cdcbd67c59a50ac33
Gabriel Morato <87160136+gpurplem@users.noreply.github.com>
2022-03-22 20:37:38 -0300

    Update conectarBanco.php

--- cadastros/conectarBanco.php
+++ cadastros/conectarBanco.php
@@ -1,4 +1,4 @@
 <?PHP
     //Instancia classe PDO. Faz a conexão com o banco.
-    $conexao = new PDO('mysql:host=localhost;dbname=provanewm', 'root', '');
+    $conexao = new PDO('mysql:host=localhost;dbname=crud', 'root', '');
 ?>
